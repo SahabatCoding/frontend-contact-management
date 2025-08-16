f3aa261896a376e23787c24408e51d4520a3b738
SahabatCoding <sahabatcoding29@gmail.com>
2025-08-16 10:43:43 +0700

    update

--- src/components/routes/AuthRoute.jsx
+++ src/components/routes/AuthRoute.jsx
@@ -1,22 +1,50 @@
-import { Navigate } from "react-router"
+import { useEffect } from "react"
+import { useNavigate } from "react-router"
 import { useLocalStorage } from "react-use"
 
 export function ProtectedRoute({ children }) {
 
     const [token, _] = useLocalStorage("token", "")
+    const navigate = useNavigate()
+
+    async function fetch() {
+        if(!token){
+            await navigate({
+                pathname : "/login"
+            })
+        }
+    }
+
+    useEffect(()=>{
+        fetch().then(()=>console.log("berhasil protect"))
+    })
 
     if (!token) {
-        return <Navigate to={"/login"} replace />
+        return null
     }
-    return children  //tidak tampilkan login
+    return children 
 }
 
 export function PublicRoute({ children }) {
 
     const [token, _] = useLocalStorage("token", "")
+    const navigate = useNavigate()
+
+    async function fetch() {
+        if(token){
+            await navigate({
+                pathname : "/dashboard/contacts"
+            })
+        }
+    }
+
+    useEffect(()=>{
+        fetch().then(()=>console.log("berhasil public"))
+    })
 
     if (token) {
-        return <Navigate to={"/dashboard/contacts"} replace />
+        return null
     }
+
     return children
 }
